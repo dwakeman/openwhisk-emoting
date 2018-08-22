f86a3558b8c4b2bb69bb2e1033cf63136f6cedc1
Dave Wakeman <dwakeman@us.ibm.com>
2018-08-21 19:27:06 -0500

    Updated apiUrl to point to my endpoint

--- docs/js/emoting.js
+++ docs/js/emoting.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/53c5e3d4519bd8dc04e7f599d1322296564124349c916705557963ca5090374c/emoting/1';
+const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/14f7ad6c22914837fecbbdbb041e8b8c3abfc79ae48be5d40d8f60309584551a/emoting/1';
 const ratingChoices = ['verygood', 'good', 'bad', 'verybad'];
 const emoting = {
   create(questionTitle) {
